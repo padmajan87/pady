1a8839fcddb80a41191049cf8df20211f9112877
padmajan <padmajan87@gmail.com>
2019-01-03 12:21:56 +0530

    junit negative test

--- src/test/java/com/example/consumer/ConsumerApplicationTests.java
+++ src/test/java/com/example/consumer/ConsumerApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.consumer;
 
 import org.junit.Test;
+import org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,6 +12,11 @@ public class ConsumerApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void test() {
+		fail("not implemented");
 	}
 
 }
